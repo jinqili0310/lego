76a51899fa8ff15ec4967bd4bd3e8ea127d96f79
Jinqi Li <jxl2047@miami.edu>
2020-08-13 04:25:27 -0400

    build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Home from './components/home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="navRoute">
+			<BrowserRouter>
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Redirect to="/" />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
